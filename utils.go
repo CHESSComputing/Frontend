905b1a04bc2de0358950e6af83d18b12dcee706d
Valentin Kuznetsov <vkuznet@gmail.com>
2024-10-26 13:37:22 -0400

    Add helper functions: chessAttributes, numberOfRecords, chunkOfRecords

--- utils.go
+++ utils.go
@@ -5,6 +5,7 @@ package main
 // Copyright (c) 2023 - Valentin Kuznetsov <vkuznet@gmail.com>
 //
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -13,6 +14,8 @@ import (
 
 	authz "github.com/CHESSComputing/golib/authz"
 	srvConfig "github.com/CHESSComputing/golib/config"
+	ldap "github.com/CHESSComputing/golib/ldap"
+	services "github.com/CHESSComputing/golib/services"
 )
 
 // helper function to get new token for given user and scope
@@ -59,4 +62,87 @@ func columnNames(attrs []string) []string {
 		out = append(out, camel)
 	}
 	return out
+}
+
+// helper function to get CHESS user attributes
+func chessAttributes(user string) (ldap.Entry, error) {
+	var attrs ldap.Entry
+
+	// obtain valid token
+	_httpReadRequest.GetToken()
+
+	// make call to Authz server to obtain user attributes
+	rurl := fmt.Sprintf("%s/attrs?user=%s", srvConfig.Config.Services.AuthzURL, user)
+	resp, err := _httpReadRequest.Get(rurl)
+	if err != nil {
+		return attrs, err
+	}
+	// parse data records from meta-data service
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return attrs, err
+	}
+	err = json.Unmarshal(data, &attrs)
+	return attrs, err
+}
+
+// helper function to obtain chunk of records for given service request
+func numberOfRecords(rec services.ServiceRequest) (int, error) {
+	var total int
+
+	// obtain valid token
+	_httpReadRequest.GetToken()
+
+	// based on user query process request from all FOXDEN services
+	data, err := json.Marshal(rec)
+	if err != nil {
+		log.Println("ERROR: marshall error", err)
+		return total, err
+	}
+	rurl := fmt.Sprintf("%s/total", srvConfig.Config.Services.DiscoveryURL)
+	resp, err := _httpReadRequest.Post(rurl, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		log.Println("ERROR: HTTP POST error", err)
+		return total, err
+	}
+	// parse data records from discovery service
+	defer resp.Body.Close()
+	data, err = io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("ERROR: IO error", err)
+		return total, err
+	}
+	err = json.Unmarshal(data, &total)
+	return total, err
+}
+
+// helper function to obtain chunk of records for given service request
+func chunkOfRecords(rec services.ServiceRequest) (services.ServiceResponse, error) {
+	var response services.ServiceResponse
+
+	// obtain valid token
+	_httpReadRequest.GetToken()
+
+	// based on user query process request from all FOXDEN services
+	data, err := json.Marshal(rec)
+	if err != nil {
+		log.Println("ERROR: marshall error", err)
+		return response, err
+	}
+	rurl := fmt.Sprintf("%s/search", srvConfig.Config.Services.DiscoveryURL)
+	resp, err := _httpReadRequest.Post(rurl, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		log.Println("ERROR: HTTP POST error", err)
+		return response, err
+	}
+	// parse data records from discovery service
+	defer resp.Body.Close()
+	data, err = io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("ERROR: IO error", err)
+		return response, err
+	}
+	err = json.Unmarshal(data, &response)
+	return response, err
 }
