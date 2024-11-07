c6a4e1f512c3f2e526c3823dd086b1e030790de1
Valentin Kuznetsov <vkuznet@gmail.com>
2024-11-07 12:54:34 -0500

    implement publishToZenodo logic

--- publish.go
+++ publish.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
+	"io"
 	"strings"
 
 	materialCommons "github.com/CHESSComputing/golib/MaterialCommons"
+	srvConfig "github.com/CHESSComputing/golib/config"
+	"github.com/CHESSComputing/golib/zenodo"
 )
 
 // helper function to publish did with given provider
@@ -29,8 +33,77 @@ func publishDataset(did, provider, description string) (string, error) {
 func publishToZenodo(did, description string) (string, error) {
 	var err error
 	var doi string
-	log.Println("call publishToZenodo", did)
-	return doi, err
+
+	// prepare http writer
+	_httpWriteRequest.GetToken()
+
+	// create new DOI resource
+	rurl := fmt.Sprintf("%s/create", srvConfig.Config.Services.PublicationURL)
+	resp, err := _httpWriteRequest.Post(rurl, "application/json", bytes.NewBuffer([]byte{}))
+	defer resp.Body.Close()
+	if err != nil {
+		return doi, err
+	}
+
+	// capture response and extract document id (did)
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return doi, err
+	}
+	var doc zenodo.CreateResponse
+	err = json.Unmarshal(data, &doc)
+	if err != nil {
+		return doi, err
+	}
+	docId := doc.Id
+
+	// create new meta-data record
+	creator := zenodo.Creator{Name: "FOXDEN", Affiliation: "Cornell University"}
+	mrec := zenodo.MetaDataRecord{
+		PublicationType: "dataset",
+		Description:     description,
+		Title:           fmt.Sprintf("FOXDEN dataset did=%s", did),
+		Licences:        []string{"MIT"},
+		Creators:        []zenodo.Creator{creator},
+	}
+	data, err = json.Marshal(mrec)
+	if err != nil {
+		return doi, err
+	}
+	rurl = fmt.Sprintf("%s/update/%d", srvConfig.Config.Services.PublicationURL, docId)
+	metaResp, err := _httpWriteRequest.Put(rurl, "application/json", bytes.NewBuffer(data))
+	defer metaResp.Body.Close()
+	if err != nil || metaResp.StatusCode != 200 {
+		return doi, err
+	}
+
+	// publish the record
+	rurl = fmt.Sprintf("%s/publish/%d", srvConfig.Config.Services.PublicationURL, docId)
+	publishResp, err := _httpWriteRequest.Post(rurl, "application/json", bytes.NewBuffer([]byte{}))
+	defer publishResp.Body.Close()
+	if err != nil || (publishResp.StatusCode < 200 || publishResp.StatusCode >= 400) {
+		return doi, err
+	}
+
+	// fetch our document
+	rurl = fmt.Sprintf("%s/docs/%d", srvConfig.Config.Services.PublicationURL, docId)
+	docsResp, err := _httpReadRequest.Get(rurl)
+	defer docsResp.Body.Close()
+	if err != nil || (docsResp.StatusCode < 200 || docsResp.StatusCode >= 400) {
+		return doi, err
+	}
+	data, err = io.ReadAll(docsResp.Body)
+	if err != nil {
+		return doi, err
+	}
+
+	// parse doi record
+	var doiRecord zenodo.DoiRecord
+	err = json.Unmarshal(data, &doiRecord)
+	if err != nil {
+		return doi, err
+	}
+	return doiRecord.Doi, err
 }
 
 // helper function to publish did into MaterialCommons
