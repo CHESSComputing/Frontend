c44789bff2d4ea999477f5fb57d8eceb27e54462
Valentin Kuznetsov <vkuznet@gmail.com>
2025-05-31 10:42:02 -0400

    Add go get for latest tag

--- get_golib.sh
+++ get_golib.sh
@@ -3,5 +3,7 @@ if [ ! -d ../golib ]; then
     cd ..
     echo "clone https://github.com/CHESSComputing/golib.git"
     git clone https://github.com/CHESSComputing/golib.git
+    latestTag=`git tag --list | tail -1`
+    go get https://github.com/CHESSComputing/golib@${latestTag}
     cd -
 fi
