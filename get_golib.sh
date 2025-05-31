aefe273a7d2a39e156440229d9a6106d7b3ea144
Valentin Kuznetsov <vkuznet@gmail.com>
2025-05-31 10:46:47 -0400

    adjust get_golib.sh for github action build

--- get_golib.sh
+++ get_golib.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 if [ ! -d ../golib ]; then
+    cwd=$PWD
     cd ..
     echo "clone https://github.com/CHESSComputing/golib.git"
     git clone https://github.com/CHESSComputing/golib.git
+    cd golib
     ls
     go mod init github.com/CHESSComputing/golib
     go mod tidy
     ls
-    cd -
+    cd $cwd
 fi
