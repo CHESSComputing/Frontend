2703b441ac902ed8bc043a5be25ed36a1b30d7db
Valentin Kuznetsov <vkuznet@gmail.com>
2025-06-01 09:56:34 -0400

    move check latest tag within get_golib.sh

--- get_golib.sh
+++ get_golib.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 if [ ! -d ../golib ]; then
+    wdir=$PWD
     cd ..
     echo "clone https://github.com/CHESSComputing/golib.git"
     git clone https://github.com/CHESSComputing/golib.git
+
+    # checkout all dependencies based on last tag
+    cd golib
+    lastTag=$(git tag --list | tail -1)
+    echo "lastTag=$lastTag"
     cd -
+
+    cd $wdir
+    for pkg in $(ls -l ../golib | grep ^d | awk '{print $9}'); do
+      echo "go get github.com/CHESSComputing/golib/$pkg@$lastTag"
+    done | /bin/sh
 fi
