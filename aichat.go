9ba4c07b3d8bdbb2a9cb18401e62c43600f125eb
Valentin Kuznetsov <vkuznet@gmail.com>
2025-11-19 14:49:08 -0500

    Convert AIChat to interface and implement ollama and tichy clients

--- aichat.go
+++ aichat.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
+	"time"
 
 	srvConfig "github.com/CHESSComputing/golib/config"
 	"github.com/CHESSComputing/golib/ollama"
 )
 
-var aiconfig *ollama.Config
+// AIChat represents generic AI chat interface
+type AIChat interface {
+	Chat(prompt string) (string, error)
+}
 
-func aichat(prompt string) (string, error) {
-	if aiconfig == nil {
-		aiconfig = &ollama.Config{
+// OllamaClient represents Ollama AI client
+type OllamaClient struct {
+	AIConfig *ollama.Config
+}
+
+// Chat implementation for OllamaClient
+func (o *OllamaClient) Chat(prompt string) (string, error) {
+	if o.AIConfig == nil {
+		o.AIConfig = &ollama.Config{
 			Host:  srvConfig.Config.AIChat.Host,
 			Port:  srvConfig.Config.AIChat.Port,
 			Model: srvConfig.Config.AIChat.Model,
 		}
-		log.Printf("configure ai chat with %+v", aiconfig)
+		log.Printf("configure ai chat with %+v", o.AIConfig)
 	}
 
-	client := ollama.NewClient(*aiconfig)
+	client := ollama.NewClient(*o.AIConfig)
 	ctx := context.Background()
 	stream := true
 	response, err := client.SendRequest(ctx, prompt, stream, nil)
@@ -28,4 +39,37 @@ func aichat(prompt string) (string, error) {
 		return "", err
 	}
 	return response, nil
+}
+
+// TichyClient represents tichy AI client
+type TichyClient struct {
+}
+
+// Chat implementation for TichyClient
+func (o *TichyClient) Chat(prompt string) (string, error) {
+
+	ctx := context.Background()
+	if srvConfig.Config.AIChat.Timeout != 0 {
+		ctx1, cancel := context.WithTimeout(
+			context.Background(),
+			time.Duration(srvConfig.Config.AIChat.Timeout)*time.Second)
+		defer cancel()
+		ctx = ctx1
+	}
+	resp, err := aitichy(ctx, prompt)
+	return resp, err
+}
+
+func aichat(prompt string) (string, error) {
+	if srvConfig.Config.AIChat.Client == "ollama" {
+		log.Println("call ollama client", prompt)
+		client := OllamaClient{}
+		return client.Chat(prompt)
+	} else if srvConfig.Config.AIChat.Client == "tichy" {
+		log.Println("call tichy client", prompt)
+		client := TichyClient{}
+		return client.Chat(prompt)
+	}
+	msg := "FOXDEN is not configured with AI assistance client"
+	return "", errors.New(msg)
 }
