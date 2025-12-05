7edc258043366a06aa95461f999181f78c733ca5
Valentin Kuznetsov <vkuznet@gmail.com>
2025-12-05 13:33:07 -0500

    format custom claims

--- utils.go
+++ utils.go
@@ -24,7 +24,12 @@ import (
 
 // helper function to get new token for given user and scope
 func newToken(user, scope string) (string, error) {
-	customClaims := authz.CustomClaims{User: user, Scope: scope, Kind: "client_credentials", Application: "FOXDEN"}
+	customClaims := authz.CustomClaims{
+		User:        user,
+		Scope:       scope,
+		Kind:        "client_credentials",
+		Application: "FOXDEN",
+	}
 	duration := srvConfig.Config.Authz.TokenExpires
 	if duration == 0 {
 		duration = 7200
